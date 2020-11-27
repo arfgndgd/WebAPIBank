b95bacecc60388c2459443e2807a1cd0250999b6
Arif Gündoğdu <gundogduarif4@gmail.com>
2020-11-27 19:57:57 +0300

    Add project files.

---
+++ WebAPIBank/Models/Context/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank.Models.Entities;
+using WebAPIBank.Models.Init;
+
+namespace WebAPIBank.Models.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            Database.SetInitializer(new MyInit()); //burada MyInit sınıfmızı tetiklemiz gerekir ki overrise ettigimiz Seed metodu burada calıssın.
+        }
+
+        public DbSet<CardInfo> Cards { get; set; }
+    }
+}
